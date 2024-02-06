dc030346c8060a2295035265ea659770bbd9cece
tainaraoliver <tr30405@gmail.com>
2024-02-06 14:50:48 -0600

    teste.js

---
+++ teste.js
@@ -0,0 +1,16 @@
+// Pseudo-Código
+let numero = prompt("Digite o numero:")
+
+
+if (numero > 0){
+    console.log("O número é positivo.");
+    soma=Number(numero)+Number(numero);
+    console.log(soma);
+    }
+
+else if (numero < 0) {
+    console.log("O número é negativo.");
+    }
+ else {
+    console.log("O número é zero.");
+    }
